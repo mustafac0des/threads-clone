3e9b0a36b8416237a5f0d7c19be4a9b0c7903fe0
mustafac0des <mustafa357yt@gmail.com>
2024-07-09 16:57:45 +0500

    Start of the new portfolio

--- reThreads/server_side/controllers/userController.js
+++ reThreads/server_side/controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
 
-const signUpUser = async (req, res) => {
+const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
     const user = await User.findOne({ $or: [{ email }, { username }] });
@@ -40,17 +40,97 @@ const signUpUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   try {
-    const { username, password } = req.body();
+    const { username, password } = req.body;
     const user = await User.findOne({ username });
-    const isPasswordCorrect = await await bcrypt.compare(
-      password,
-      user.password
-    );
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
     if (!user || !isPasswordCorrect) {
       return res.status(400).json({ message: "Invalid!" });
     }
-  } catch (err) {}
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+    });
+  } catch (err) {
+    console.error("Error in sign up user:", err);
+    res.status(500).json({ message: err.message });
+  }
 };
 
-export default signUpUser;
+const logoutUser = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "Logged out!" });
+  } catch (err) {
+    console.error("Error in sign up user:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const followUnfollow = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id) {
+      return res.status(400).json({ message: "You cannot follow yourself" });
+    }
+
+    if (!userToModify || !currentUser) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const modifyFollowings = currentUser.following.includes(id);
+
+    if (modifyFollowings) {
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      res.status(200).json({ message: "User followed successfully!" });
+    } else {
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      res.status(200).json({ message: "User un-followed successfully!" });
+    }
+  } catch (err) {
+    console.error("Error in follow/unfollow:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { name, email, username, password, profilePic, biography } = req.body;
+  const userId = req.user._id;
+
+  try {
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User not found!" });
+    }
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.biography = biography || user.biography;
+
+    user = await user.save();
+    res.status(200).json({ message: "Profile updated successfully => ", user });
+  } catch (err) {
+    console.error("Error in updating profile", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export { updateUser, loginUser, logoutUser, followUnfollow, signupUser };
--- reThreads/server_side/routes/userRoutes.js
+++ reThreads/server_side/routes/userRoutes.js
@@ -1,14 +1,20 @@
 import express from "express";
-import signUpUser from "../controllers/userController.js";
+import {
+  loginUser,
+  logoutUser,
+  signupUser,
+  followUnfollow,
+  updateUser,
+} from "../controllers/userController.js";
+
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
-function okayconsolethis() {
-  console.log("heya!");
-}
-
-okayconsolethis();
-
-router.post("/signup", signUpUser);
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/update/:id", protectRoute, updateUser);
+router.post("/followUnfollow/:id", protectRoute, followUnfollow);
 
 export default router;
